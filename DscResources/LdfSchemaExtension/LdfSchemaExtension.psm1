8ca76fe70108c36a5744f9816b75e8d6f593a860
chasewilson <Chase.wilson@microsoft.com>
2018-01-25 12:15:18 -0700

    Updates to error handling to allow for language files

--- DscResources/LdfSchemaExtension/LdfSchemaExtension.psm1
+++ DscResources/LdfSchemaExtension/LdfSchemaExtension.psm1
@@ -2,24 +2,8 @@ Import-Module -Name (Join-Path -Path ( Split-Path $PSScriptRoot -Parent ) `
                                -ChildPath 'SchemaExtensionResourceHelper\LdfSchemaExtensionHelper.psm1') `
                                -Force
 
-# Localized messages
-data LocalizedData
-{
-    # culture="en-US"
-    ConvertFrom-StringData -StringData @'
-        ErrorPathNotFound = The requested path "{0}" cannot be found.
-        ExtensionAttributeIdNotFound = Error obtaining Schema Extension Object with attribute id "{0}"
-        ExtensionGovernsIdNotFound = Error obtaining Schema Extension Object with governs id "{0}"
-        ExtensionObjectFound = Obtained "{0}" Schema Extension Object
-        DistinguishedName = Configuration Distinguished Name "{0}" does not match current Domain Distinguished Name "{1}"
-        MayContainfound = Obtained "{0}" in "{1}"
-        MayContainNotFound = "{0}" was not found in specified Schema Object
-        AsciiConversion = Converting Schema .ldf to ASCII format
-        FileEncoding = Getting the Schema .ldf encoding
-        ldifde = Setting schema extension.
-        ldifdeErrorLog = "{0}"
-'@
-}
+# Localized messages for Write-Verbose statements in this resource
+$script:localizedData = Get-LocalizedData -ResourceName 'LdfSchemaExtension'
 
 Function Get-TargetResource
 {
---
+++ DscResources/LdfSchemaExtension/en-US/LdfSchemaExtension.strings.psd1
@@ -0,0 +1,13 @@
+ConvertFrom-StringData -StringData @'
+    ErrorPathNotFound = The requested path "{0}" cannot be found.
+    ExtensionAttributeIdNotFound = Error obtaining Schema Extension Object with attribute id "{0}"
+    ExtensionGovernsIdNotFound = Error obtaining Schema Extension Object with governs id "{0}"
+    ExtensionObjectFound = Obtained "{0}" Schema Extension Object
+    DistinguishedName = Configuration Distinguished Name "{0}" does not match current Domain Distinguished Name "{1}"
+    MayContainfound = Obtained "{0}" in "{1}"
+    MayContainNotFound = "{0}" was not found in specified Schema Object
+    AsciiConversion = Converting Schema .ldf to ASCII format
+    FileEncoding = Getting the Schema .ldf encoding
+    ldifde = Setting schema extension.
+    ldifdeErrorLog = "{0}"
+'@
